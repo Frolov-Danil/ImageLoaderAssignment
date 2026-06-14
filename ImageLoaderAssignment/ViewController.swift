c8ec45d045d444b3c5226a7b4560c8ced3588061
Danil <danil.frolov@amedia.ua>
2026-06-14 14:29:01 +0400

    Initial Commit

---
+++ ImageLoaderAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageLoaderAssignment
+//
+//  Created by Danil Frolov on 14.06.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
